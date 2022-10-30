540482bd4a93fb3f8b7de15b5f92a40f07ec110c
Mian Sarim Hameed <miansarimhameed@gmail.com>
2022-10-30 17:52:44 +0500

    Update App Resources :lipstick:

--- app/src/main/java/com/miansarimhameed/calorietracker/ui/theme/Color.kt
+++ app/src/main/java/com/miansarimhameed/calorietracker/ui/theme/Color.kt
@@ -2,7 +2,13 @@ package com.miansarimhameed.calorietracker.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val BrightGreen = Color(0xFF00C713)
+val DarkGreen = Color(0xFF00790C)
+val Orange =  Color(0xFFFFAA00)
+val CarbColor = Color(0xFFEEFF00)
+val ProteinColor = Orange
+val FatColor = Color(0xFFF44336)
+val LightGray = Color(0xFF808080)
+val MediumGray = Color(0xFF404040)
+val DarkGray = Color(0xFF202020)
+val TextWhite = Color(0xFFEEEEEE)
--- app/src/main/java/com/miansarimhameed/calorietracker/ui/theme/Theme.kt
+++ app/src/main/java/com/miansarimhameed/calorietracker/ui/theme/Theme.kt
@@ -5,26 +5,30 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = BrightGreen,
+    primaryVariant = DarkGreen,
+    secondary = Orange,
+    background = MediumGray,
+    onBackground = TextWhite,
+    surface = LightGray,
+    onSurface = TextWhite,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
+    primary = BrightGreen,
+    primaryVariant = DarkGreen,
+    secondary = Orange,
     background = Color.White,
+    onBackground = DarkGray,
     surface = Color.White,
+    onSurface = DarkGray,
     onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    onSecondary = Color.White,
 )
 
 @Composable
