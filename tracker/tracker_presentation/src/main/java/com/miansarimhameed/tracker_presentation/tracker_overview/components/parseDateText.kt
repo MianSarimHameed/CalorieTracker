8fcfa77e2b277ec94cf07bc0b4a1d8db8765e755
Mian Sarim Hameed <miansarimhameed@gmail.com>
2022-11-07 18:14:22 +0500

    Implement Day Selector :sparkles:

---
+++ tracker/tracker_presentation/src/main/java/com/miansarimhameed/tracker_presentation/tracker_overview/components/parseDateText.kt
@@ -0,0 +1,18 @@
+package com.miansarimhameed.tracker_presentation.tracker_overview.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.miansarimhameed.core.R
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+@Composable
+fun parseDateText(date: LocalDate): String {
+    val today = LocalDate.now()
+    return when(date) {
+        today -> stringResource(id = R.string.today)
+        today.minusDays(1) -> stringResource(id = R.string.yesterday)
+        today.plusDays(1) -> stringResource(id = R.string.tomorrow)
+        else -> DateTimeFormatter.ofPattern("dd LLLL").format(date)
+    }
+}
